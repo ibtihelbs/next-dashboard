0347db914f5c6cf6b83e9954278e690d405c1f52
ibtihelbs <ibtihel.bensalah@outlook.fr>
2023-11-09 15:19:22 +0100

    run prod

---
+++ app/lib/actions.tsx
@@ -0,0 +1,81 @@
+"use server";
+import { z } from "zod";
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { signIn } from "@/auth";
+const InvoiceSchema = z.object({
+  id: z.string().nullish(),
+  customerId: z.string().nullish(),
+  amount: z.coerce.number(),
+  status: z.enum(["pending", "paid"]).nullish(),
+  date: z.string().nullish(),
+});
+const CreateInvoice = InvoiceSchema.omit({ id: true, date: true });
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
+
+  try {
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+    revalidatePath("/dashboard/invoices");
+    redirect("/dashboard/invoices");
+  } catch (error) {
+    console.log(error);
+  }
+}
+const UpdateInvoice = InvoiceSchema.omit({ date: true });
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  const amountInCents = amount * 100;
+
+  try {
+    await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+    `;
+    revalidatePath("/dashboard/invoices");
+    redirect("/dashboard/invoices");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function deleteInvoice(id: string) {
+  //throw new Error("Failed to Delete Invoice");
+
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath("/dashboard/invoices");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", Object.fromEntries(formData));
+  } catch (error) {
+    if ((error as Error).message.includes("CredentialsSignin")) {
+      return "CredentialSignin";
+    }
+    throw error;
+  }
+}
